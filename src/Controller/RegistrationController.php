27afa66b21e9d450fcc80fc5ee85e31f9de2a97d
Yoann Leonard <yoann.leonard@gmail.com.com>
2022-11-25 18:22:00 +0100

    added getRegistrationDetails function

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Event;
+use App\Repository\EventRepository;
 use App\Repository\RegistrationRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -13,9 +14,33 @@ use Symfony\Component\Serializer\SerializerInterface;
 class RegistrationController extends AbstractController
 {
     #[Route('/api/registrations/{id}', name: 'registration_list', methods: ['GET'])]
-    public function getRegistrationList(Event $event, RegistrationRepository $registrationRepository, SerializerInterface $serializer): JsonResponse
+    public function getRegistrationList(Event $event, SerializerInterface $serializer): JsonResponse
     {
         $jsonRegistration = $serializer->serialize($event, 'json', ['groups' => 'getRegistrations']);
         return new JsonResponse($jsonRegistration, Response::HTTP_OK, ['accept' => 'json'], true);
+    }
+
+    #[Route('/api/registrations/{idEvent}/{idRegistration}', name: 'registration_details', methods: ['GET'])]
+    public function getRegistrationDetails(
+        int $idEvent, int $idRegistration, RegistrationRepository $registrationRepository, EventRepository $eventRepository, SerializerInterface $serializer
+    ): JsonResponse
+    {
+        $registration = $registrationRepository->find($idRegistration);
+        $event = $eventRepository->find($idEvent);
+        
+        if (!$event) {
+            return new JsonResponse(null, Response::HTTP_NOT_FOUND);
+        }
+        
+        // check if event and registration are linked
+        if (!$event->getRegistrations()->contains($registration)) {
+            return new JsonResponse(null, Response::HTTP_NOT_FOUND);
+        }
+
+        if ($registration) {
+            $jsonRegistration = $serializer->serialize($registration, 'json', ['groups' => 'getRegistrations']);
+            return new JsonResponse($jsonRegistration, Response::HTTP_OK, [], true);
+        }
+        return new JsonResponse(null, Response::HTTP_NOT_FOUND);
     }
 }
