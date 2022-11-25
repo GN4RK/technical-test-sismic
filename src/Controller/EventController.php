86f006f2b33a356102687dbbdbd4b57a6851b7da
Yoann Leonard <yoann.leonard@gmail.com.com>
2022-11-25 17:21:08 +0100

    added DELETE CREATE and UPDATE functions

--- src/Controller/EventController.php
+++ src/Controller/EventController.php
@@ -4,10 +4,14 @@ namespace App\Controller;
 
 use App\Entity\Event;
 use App\Repository\EventRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
 use Symfony\Component\Serializer\SerializerInterface;
 
 class EventController extends AbstractController
@@ -26,6 +30,52 @@ class EventController extends AbstractController
     {
         $jsonEvent = $serializer->serialize($event, 'json', ['groups' => 'getEvents']);
         return new JsonResponse($jsonEvent, Response::HTTP_OK, ['accept' => 'json'], true);
+    }
+    
+    #[Route('/api/events/{id}', name: 'delete_event', methods: ['DELETE'])]
+    public function deleteEvent(Event $event, EntityManagerInterface $em): JsonResponse
+    {
+        $em->remove($event);
+        $em->flush();
+
+        return new JsonResponse(null, Response::HTTP_NO_CONTENT);
+    }
+    
+    #[Route('/api/events', name: 'create_event', methods: ['POST'])]
+    public function createEvent(
+        Request $request, SerializerInterface $serializer, EntityManagerInterface $em, 
+        UrlGeneratorInterface $urlGenerator
+        ): JsonResponse
+    {
+        $event = $serializer->deserialize($request->getContent(), Event::class, 'json');
+        $em->persist($event);
+        $em->flush();
+
+        $jsonEvent = $serializer->serialize($event, 'json', ['groups' => 'getEvents']);
+        $location = $urlGenerator->generate(
+            'event_details', ['id' => $event->getId()], UrlGeneratorInterface::ABSOLUTE_URL
+        );
+
+        return new JsonResponse($jsonEvent, Response::HTTP_CREATED, ['location' => $location], true);
+    }
+    
+    #[Route('/api/events/{id}', name: 'update_event', methods: ['PUT'])]
+    public function updateEvent(
+        Request $request, SerializerInterface $serializer, Event $currentEvent, 
+        EntityManagerInterface $em, UrlGeneratorInterface $urlGenerator
+        ): JsonResponse
+    {
+        $updatedEvent = $serializer->deserialize(
+            $request->getContent(), 
+            Event::class, 
+            'json',
+            [AbstractNormalizer::OBJECT_TO_POPULATE => $currentEvent]
+        );
+
+        $em->persist($updatedEvent);
+        $em->flush();
+
+        return new JsonResponse(null, JsonResponse::HTTP_NO_CONTENT);
     }
 
 
