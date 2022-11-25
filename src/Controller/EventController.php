fe3fd092e63e8f0370d6d54f158a6f88ad868efa
Yoann Leonard <yoann.leonard@gmail.com.com>
2022-11-25 16:25:50 +0100

    added event controller with event_list and event_details function

---
+++ src/Controller/EventController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Event;
+use App\Repository\EventRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\SerializerInterface;
+
+class EventController extends AbstractController
+{
+    #[Route('/api/events', name: 'event_list', methods: ['GET'])]
+    public function getEventList(EventRepository $eventRepository, SerializerInterface $serializer): JsonResponse
+    {
+        $eventList = $eventRepository->findAll();
+        $jsonEventList = $serializer->serialize($eventList, 'json');
+
+        return new JsonResponse($jsonEventList, Response::HTTP_OK, [], true);
+    }
+    
+    #[Route('/api/events/{id}', name: 'event_details', methods: ['GET'])]
+    public function getEvent(Event $event, SerializerInterface $serializer): JsonResponse
+    {
+        $jsonEvent = $serializer->serialize($event, 'json');
+        return new JsonResponse($jsonEvent, Response::HTTP_OK, ['accept' => 'json'], true);
+    }
+
+
+}
